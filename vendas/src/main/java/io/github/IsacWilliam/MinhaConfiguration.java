7d20b63485c15dfab61ca2e9082ef6dd4c903114
Isac William <hailisac@hotmail.com>
2023-04-26 18:36:18 -0300

    Seção 2 /Aula 12 - Trabalhando com multiplos ambientes e profiles

--- vendas/src/main/java/io/github/IsacWilliam/MinhaConfiguration.java
+++ vendas/src/main/java/io/github/IsacWilliam/MinhaConfiguration.java
@@ -6,12 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
-    @Bean(name = "outraConfiguração")
-    public String outraConfiguracao(){
-        return "Sitema de Vendas";
-    }
 }
