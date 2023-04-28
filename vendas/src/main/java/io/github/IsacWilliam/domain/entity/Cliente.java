25bc21fc29c4051addf6ab14fb8d9efcf38d4d86
Isac William <hailisac@hotmail.com>
2023-04-28 17:32:01 -0300

    Seção 3 / Aula 19 - Concluindo o Cadastro de Clientes

--- vendas/src/main/java/io/github/IsacWilliam/domain/entity/Cliente.java
+++ vendas/src/main/java/io/github/IsacWilliam/domain/entity/Cliente.java
@@ -2,22 +2,37 @@ package io.github.IsacWilliam.domain.entity;
 
 public class Cliente {
 
-    private Integer ig;
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    private Integer id;
     private String nome;
-
-    public Integer getIg() {
-        return ig;
+    public Integer getId() {
+        return id;
     }
-
-    public void setIg(Integer ig) {
-        this.ig = ig;
+    public void setId(Integer id) {
+        this.id = id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
     }
 }
