f232076ff1ec5f561e75f64f30a522abc9089d0f
Isac William <hailisac@hotmail.com>
2023-04-28 15:03:04 -0300

    Seção 3 / Aula 18 Criando as Classes de Modelo

---
+++ vendas/src/main/java/io/github/IsacWilliam/domain/entity/Cliente.java
@@ -0,0 +1,23 @@
+package io.github.IsacWilliam.domain.entity;
+
+public class Cliente {
+
+    private Integer ig;
+    private String nome;
+
+    public Integer getIg() {
+        return ig;
+    }
+
+    public void setIg(Integer ig) {
+        this.ig = ig;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
