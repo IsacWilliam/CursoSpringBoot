9d78eaf0c126995a7a6b1cc442c748b15cdc255d
Isac William <hailisac@hotmail.com>
2023-05-05 09:42:11 -0300

    Seção 3 / Aula 30 - Mapeando a Entidade ItemPedido

--- vendas/src/main/java/io/github/IsacWilliam/domain/entity/Pedido.java
+++ vendas/src/main/java/io/github/IsacWilliam/domain/entity/Pedido.java
@@ -3,6 +3,7 @@ package io.github.IsacWilliam.domain.entity;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "pedido")
@@ -22,6 +23,16 @@ public class Pedido {
     @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
 
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
 
     public Integer getId() {
         return id;
