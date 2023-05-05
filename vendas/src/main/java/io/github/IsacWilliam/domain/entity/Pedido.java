365d722584fe1663a8e5250d01798a435d287926
Isac William <hailisac@hotmail.com>
2023-05-05 09:28:33 -0300

    Seção 3 / Aula 29 - Mapeando a Entidade Pedido

--- vendas/src/main/java/io/github/IsacWilliam/domain/entity/Pedido.java
+++ vendas/src/main/java/io/github/IsacWilliam/domain/entity/Pedido.java
@@ -1,12 +1,25 @@
 package io.github.IsacWilliam.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pedido")
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "dataPedido")
     private LocalDate dataPedido;
+
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
 
 
