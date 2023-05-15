5a6412aa4d151cf2214429d2cb1d5659121cb6b8
Isac William <hailisac@hotmail.com>
2023-05-15 17:48:24 -0300

    Seção 4:API's / Aula 39 - Requisição POST e Request Body

--- vendas/src/main/java/io/github/IsacWilliam/VendasApplication.java
+++ vendas/src/main/java/io/github/IsacWilliam/VendasApplication.java
@@ -1,22 +1,11 @@
 package io.github.IsacWilliam;
 
-import io.github.IsacWilliam.domain.entity.Cliente;
-import io.github.IsacWilliam.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
-        @Bean
-        public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
-            return args -> {
-                Cliente c = new Cliente(null, "isac william");
-                clientes.save(c);
-            };
-        }
+
         public static void main(String[] args) {
             SpringApplication.run(VendasApplication.class, args);
         }
--- vendas/src/main/java/io/github/IsacWilliam/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/IsacWilliam/rest/controller/ClienteController.java
@@ -15,18 +15,25 @@ public class ClienteController {
 
     private Clientes clientes;
 
-    public ClienteController (Clientes clientes){
+    public ClienteController(Clientes clientes) {
         this.clientes = clientes;
     }
 
     @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id){
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientes.findById(id);
 
-        if (cliente.isPresent()){
+        if (cliente.isPresent()) {
             return ResponseEntity.ok(cliente.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente) {
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
     }
 }
