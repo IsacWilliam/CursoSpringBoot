e367df09d240ddf44b8a7fc49f03ddbb52a6cdc2
Isac William <hailisac@hotmail.com>
2023-05-18 16:45:12 -0300

    Seção 4: API's / Aula 51 - Método para Realização de um Pedido.

--- vendas/src/main/java/io/github/IsacWilliam/rest/controller/PedidoController.java
+++ vendas/src/main/java/io/github/IsacWilliam/rest/controller/PedidoController.java
@@ -1,9 +1,11 @@
 package io.github.IsacWilliam.rest.controller;
 
+import io.github.IsacWilliam.domain.entity.Pedido;
+import io.github.IsacWilliam.rest.dto.PedidoDTO;
 import io.github.IsacWilliam.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.*;
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
@@ -12,4 +14,11 @@ public class PedidoController {
     public PedidoController(PedidoService service) {
         this.service = service;
     }
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Integer save (@RequestBody PedidoDTO dto){
+        Pedido pedido = service.salvar(dto);
+        return pedido.getId();
+    }
+
 }
--- vendas/src/main/java/io/github/IsacWilliam/service/PedidoService.java
+++ vendas/src/main/java/io/github/IsacWilliam/service/PedidoService.java
@@ -1,4 +1,8 @@
 package io.github.IsacWilliam.service;
 
+import io.github.IsacWilliam.domain.entity.Pedido;
+import io.github.IsacWilliam.rest.dto.PedidoDTO;
+
 public interface PedidoService {
+    Pedido salvar(PedidoDTO dto);
 }
