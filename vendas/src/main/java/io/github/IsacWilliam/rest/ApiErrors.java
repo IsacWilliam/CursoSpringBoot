4ae8484c16c9e699e9bcb56149cd335e84b87043
Isac William <hailisac@hotmail.com>
2023-05-24 12:18:37 -0300

    Seção 5: Bean Validation / Aula 60 - Utilizando e Testando o Bean Validation

--- vendas/src/main/java/io/github/IsacWilliam/rest/ApiErrors.java
+++ vendas/src/main/java/io/github/IsacWilliam/rest/ApiErrors.java
@@ -7,7 +7,11 @@ import java.util.List;
 
 public class ApiErrors {
     @Getter
-    private final List<String> errors;
+    private List<String> errors;
+
+    public ApiErrors(List<String> errors) {
+        this.errors = errors;
+    }
 
     public ApiErrors(String mensagemErro) {
         this.errors = Collections.singletonList(mensagemErro);
