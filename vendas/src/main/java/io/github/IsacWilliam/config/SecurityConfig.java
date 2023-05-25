ea8abb3f80814fc15f2f6c2b473d971894aeb795
Isac William <hailisac@hotmail.com>
2023-05-25 15:31:22 -0300

    Seção 6: Spring Security e JWT / Aula 71 - Configurando a Autorização de URL's.

--- vendas/src/main/java/io/github/IsacWilliam/config/SecurityConfig.java
+++ vendas/src/main/java/io/github/IsacWilliam/config/SecurityConfig.java
@@ -27,6 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/api/clientes/**")
+                //.permitAll()
+                //OU .authenticated() OU AINDA
+                .hasRole("USER")
+                .and()
+                .formLogin();
     }
 }
