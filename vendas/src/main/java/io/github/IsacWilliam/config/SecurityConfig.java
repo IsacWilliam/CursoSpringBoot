951543e38d64a8df09541282942a9a7e51f6c671
Isac William <hailisac@hotmail.com>
2023-05-27 14:22:53 -0300

    Seção 6: Spring Security e JWT / Aula 81 - Finalizando Configuração do JWT no Spring Security.

--- vendas/src/main/java/io/github/IsacWilliam/config/SecurityConfig.java
+++ vendas/src/main/java/io/github/IsacWilliam/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.IsacWilliam.config;
 
+import io.github.IsacWilliam.security.jwt.JwtAuthFilter;
+import io.github.IsacWilliam.security.jwt.JwtService;
 import io.github.IsacWilliam.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,17 +10,28 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UsuarioServiceImpl usuarioService;
 
+    @Autowired
+    private JwtService jwtService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, usuarioService);
     }
 
     @Override
@@ -44,8 +57,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                 .anyRequest()
                 .authenticated()
-                .and()
-                .httpBasic();
-
+            .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+        ;
     }
 }
