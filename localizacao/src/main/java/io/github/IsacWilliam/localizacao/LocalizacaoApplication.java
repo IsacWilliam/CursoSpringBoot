f4959cb8a667b81998833a1c4438cd84b0a5aacd
Isac William <hailisac@hotmail.com>
2023-06-02 17:16:42 -0300

    Seção 12: Aprofundando em Spring Data JPA / Aula 97 - Criando e Testando o Repositório de Cidade.

--- localizacao/src/main/java/io/github/IsacWilliam/localizacao/LocalizacaoApplication.java
+++ localizacao/src/main/java/io/github/IsacWilliam/localizacao/LocalizacaoApplication.java
@@ -1,17 +1,34 @@
 package io.github.IsacWilliam.localizacao;
 
+import io.github.IsacWilliam.localizacao.domain.entity.Cidade;
+import io.github.IsacWilliam.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-
+		salvarCidade();
+		listarCidades();
 	}
 
+	@Transactional
+	void salvarCidade(){
+		var cidade = new Cidade(1L, "Sao Paulo", 12396372L);
+		cidadeRepository.save(cidade);
+	}
+
+	void listarCidades(){
+		cidadeRepository.findAll().forEach(System.out::println);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LocalizacaoApplication.class, args);
 	}
