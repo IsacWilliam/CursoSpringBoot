4021e19eda0715b29f9936e85d97994af4292654
Isac William <hailisac@hotmail.com>
2023-06-02 13:28:19 -0300

    Seção 12: Aprofundando em Spring Data JPA / Aula 95 - Criação do Projeto Localização.

---
+++ localizacao/src/test/java/io/github/IsacWilliam/localizacao/LocalizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.IsacWilliam.localizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
