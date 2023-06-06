18836e4d96a285ed3a24b2f3d24502afbaa6c6fd
Isac William <hailisac@hotmail.com>
2023-06-06 11:41:14 -0300

    Seção 13: Trabalhando com Docker / Aula 108 - Criando uma Imagem e Rodando em Container.

---
+++ springdocker/src/test/java/io/github/IsacWilliam/springdocker/SpringdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.IsacWilliam.springdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
